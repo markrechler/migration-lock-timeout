06dec40ccc60294f03f1c4bc54cf6ef92ee08088
Mark Rechler <mrechler@justworks.com>
2024-11-15 12:30:30 -0500

    feat: add support for MySQL

--- lib/migration_lock_timeout/lock_manager.rb
+++ lib/migration_lock_timeout/lock_manager.rb
@@ -5,9 +5,14 @@ module MigrationLockTimeout
       timeout_disabled = self.class.disable_lock_timeout
       time = self.class.lock_timeout_override ||
         MigrationLockTimeout.try(:config).try(:default_timeout)
+      adapter = ActiveRecord::Base.connection_db_config.adapter
       if !timeout_disabled && direction == :up && time && !disable_ddl_transaction
         safety_assured? do
-          execute "SET LOCAL lock_timeout = '#{time}s'"
+          if adapter.include?("mysql")
+            execute "SET SESSION lock_wait_timeout = #{time}"
+          else
+            execute "SET LOCAL lock_timeout = '#{time}s'"
+          end
         end
       end
       super
